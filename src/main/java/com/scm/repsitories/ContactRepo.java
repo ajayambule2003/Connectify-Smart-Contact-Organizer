fc43457087d73e16601c076e6fade8470d122f6e
Ajay Ambule <ajayambule2003@gmail.com>
2024-12-09 22:07:32 +0530

    UserRepo file updated:

--- src/main/java/com/scm/repsitories/ContactRepo.java
+++ src/main/java/com/scm/repsitories/ContactRepo.java
@@ -1,6 +1,6 @@
 package com.scm.repsitories;
 
-import java.util.*;
+import java.util.List;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -10,7 +10,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.scm.entities.Contact;
-import com.scm.entities.User;
+import com.scm.entities.User; 
 
 @Repository
 public interface ContactRepo extends JpaRepository<Contact, String> {
